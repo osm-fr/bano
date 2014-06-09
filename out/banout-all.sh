35d7497362393520636e2c79f583443cbe412ac0
vdct <vdct@laposte.net>
2014-06-09 15:12:50 +0200

    outils d'export

--- out/banout-all.sh
+++ out/banout-all.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-for dep in {01..19} 2A 2B {30..95} {971..974}
+for dep in {01..19} 2A 2B {21..95} {971..974}
 do
 	echo "Export du $dep"
 	sh banout.sh $dep
--- out/head.sh
+++ out/head.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-for dep in {21..29}
+for dep in {01..19} 2A 2B {21..95} {971..974}
 do
 	echo $dep
-	sh banout.sh $dep
+#	sh banout.sh $dep
 done
