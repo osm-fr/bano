838b292d2f6f10e6e998452dd4221c69b80ce7e7
vdct <vdct@laposte.net>
2014-08-07 00:24:18 +0200

    Fantoir sur surfaces ou points (issues #14 #21 #43)

--- sql/highway_insee.sql
+++ sql/highway_insee.sql
@@ -1,4 +1,15 @@
-SELECT	l.name,l.tags->'ref:FR:FANTOIR' f,l.tags->'ref:FR:FANTOIR:left' fl,l.tags->'ref:FR:FANTOIR:right' fr
+SELECT	pl.name,pl.tags->'ref:FR:FANTOIR' f,'' fl,'' fr,'' suffixe
+		FROM	planet_osm_polygon 	p
+		JOIN	planet_osm_point 	pl
+		ON		pl.way && p.way					AND
+				ST_Intersects(pl.way, p.way)
+		WHERE	p.tags ? 'ref:INSEE'			AND
+				p.tags->'ref:INSEE'='__com__'	AND
+				pl.tags->'ref:FR:FANTOIR'!=''			AND
+				pl.name 	IS NOT NULL
+				group by 1,2,3,4
+UNION
+SELECT	l.name,l.tags->'ref:FR:FANTOIR' f,l.tags->'ref:FR:FANTOIR:left' fl,l.tags->'ref:FR:FANTOIR:right' fr,'' suffixe
 		FROM	planet_osm_polygon 	p
 		JOIN	planet_osm_line 	l
 		ON		ST_Intersects(l.way, p.way)
@@ -8,14 +19,14 @@ SELECT	l.name,l.tags->'ref:FR:FANTOIR' f,l.tags->'ref:FR:FANTOIR:left' fl,l.tags
 				l.name 		IS NOT NULL
 				group by 1,2,3,4
 UNION
-SELECT	pl.name,pl.tags->'ref:FR:FANTOIR' f,pl.tags->'ref:FR:FANTOIR:left' fl,pl.tags->'ref:FR:FANTOIR:right' fr
+SELECT	pl.name,pl.tags->'ref:FR:FANTOIR' f,'' fl,'' fr,'' suffixe
 		FROM	planet_osm_polygon 	p
 		JOIN	planet_osm_polygon 	pl
 		ON		pl.way && p.way					AND
 				ST_Intersects(pl.way, p.way)
 		WHERE	p.tags ? 'ref:INSEE'			AND
 				p.tags->'ref:INSEE'='__com__'	AND
-				pl.highway 	IS NOT NULL			AND
+				coalesce(pl.highway,pl.tags->'ref:FR:FANTOIR') 	IS NOT NULL			AND
 				pl.name 	IS NOT NULL
 				group by 1,2,3,4
 				;
