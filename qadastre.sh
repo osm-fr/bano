5b12940e80ca5204266b2ec95a53309485156bb0
vdct <vdct@laposte.net>
2014-08-20 20:52:55 +0200

    import des buildings - option par département (#50)

--- qadastre.sh
+++ qadastre.sh
@@ -6,6 +6,6 @@ bano_cache=/data/work/cadastre.openstreetmap.fr/bano_cache
 echo  $bano_cache/$1/$2
 
 cd $bano_cache/$1/$2
-echo "$3"
-$qadastre2OSM --download $1 $2 "$3"
+# echo "$3"
+# $qadastre2OSM --download $1 $2 "$3"
 $qadastre2OSM --convert $2 "$3"
