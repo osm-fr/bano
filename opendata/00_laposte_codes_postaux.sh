7dac65ae4b2b09a043911c7672fa5530167ec028
vdct <vdct@laposte.net>
2015-03-07 18:19:49 +0100

    ajout BIHOREL

--- opendata/00_laposte_codes_postaux.sh
+++ opendata/00_laposte_codes_postaux.sh
@@ -5,3 +5,7 @@ iconv -f iso8859-1 -t utf8 code_postaux_v201410.csv > code_postaux_v201410-utf.c
 psql cadastre -c "create table if not exists laposte_cp (insee text, commune text, cp text, libelle text); truncate table laposte_cp;"
 psql cadastre -c "\copy laposte_cp from 'code_postaux_v201410-utf.csv' with (format csv, delimiter ';', quote '>', header true);"
 psql cadastre -c "UPDATE laposte_cp SET commune=trim(commune), libelle=trim(libelle);"
+
+# ajout de BIHOREL (manquant dans fichier La Poste)
+psql cadastre -c "insert into laposte_cp values ('76095','BIHOREL','76420','BIHOREL');"
+
