f9d59712ed5f7440fa020112ce4a6ce8d55c8716
vdct <vdct@laposte.net>
2015-03-07 18:13:34 +0100

    script import codes postaux La Poste (opendata/LO)

---
+++ opendata/00_laposte_codes_postaux.sh
@@ -0,0 +1,7 @@
+# récupération et décompression du fichier FANTOIR
+wget https://www.data.gouv.fr/s/resources/base-officielle-des-codes-postaux/20141106-120608/code_postaux_v201410.csv
+iconv -f iso8859-1 -t utf8 code_postaux_v201410.csv > code_postaux_v201410-utf.csv
+# import dans SQL en format fixe (delimiter et quote spéciaux pour ignorer)
+psql cadastre -c "create table if not exists laposte_cp (insee text, commune text, cp text, libelle text); truncate table laposte_cp;"
+psql cadastre -c "\copy laposte_cp from 'code_postaux_v201410-utf.csv' with (format csv, delimiter ';', quote '>', header true);"
+psql cadastre -c "UPDATE laposte_cp SET commune=trim(commune), libelle=trim(libelle);"
