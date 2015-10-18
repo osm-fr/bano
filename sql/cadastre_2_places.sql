9eb35bc173ad1bd872526a2e737568c354d9c75f
vdct <vdct@laposte.net>
2015-10-18 23:02:33 +0200

    SQL : nodes Place bis (#50)

--- sql/cadastre_2_places.sql
+++ sql/cadastre_2_places.sql
@@ -1,6 +1,6 @@
-CREATE TEMP TABLE	buildings_noms
+WITH	buildings_noms
 AS
-SELECT	b.geometrie,	
+(SELECT	b.geometrie,	
 		b.wall,
 		p.insee_com,
 		p.voie_cadastre
@@ -9,57 +9,21 @@ JOIN	parcelles p
 ON	p.geometrie && b.geometrie	AND
 	ST_Contains(p.geometrie,st_centroid(b.geometrie))
 WHERE	p.insee_com = '#com__'	AND
-		b.wall = 'yes';
-
-/*CREATE TEMP TABLE	noms_insee
+		b.wall = 'yes'),
+noms_fantoir
 AS
-SELECT	voie_cadastre,
-		insee_com
-FROM	parcelles
-WHERE	insee_com = '#com__'	AND
-		voie_cadastre != ''
-EXCEPT
-SELECT	voie_cadastre,
-		insee_com
-FROM	parcelles
-WHERE	insee_com = '#com__'	AND
-		numero != '';
-*/
-CREATE TEMP TABLE	noms_fantoir
-AS
-SELECT	code_insee||id_voie||cle_rivoli fantoir,
+(SELECT	code_insee||id_voie||cle_rivoli fantoir,
 		libelle_voie,
 		ld_bati
 FROM	fantoir_voie
 WHERE	code_insee = '#com__'	AND
 		type_voie = '3'	AND
-		caractere_annul = '';
-/*
-DELETE FROM	cumul_places
-WHERE 		source = 'CADASTRE'	AND
-			insee_com = '#com__';
-
-INSERT INTO cumul_places (insee_com,libelle_cadastre,source,fantoir,geometrie)
-SELECT	p.insee_com,
-		p.voie_cadastre,
-		'CADASTRE',
-		coalesce(n.fantoir,null),
-		coalesce(ST_Transform(ST_Centroid(ST_Collect(b.geometrie)),4326),ST_Transform(ST_Centroid(ST_Collect(p.geometrie)),4326))
-FROM	parcelles p
-LEFT OUTER JOIN buildings_noms b
-ON		p.voie_cadastre = b.voie_cadastre
-JOIN	noms_fantoir n
---LEFT OUTER JOIN	noms_fantoir n
-ON		trim(both from substr(p.voie_cadastre,1,26)) = n.libelle_voie
-WHERE p.insee_com = '#com__'
-GROUP BY 1,2,3,4;
-
-COMMIT;*/
-
+		caractere_annul = '')
 SELECT ST_X(geometrie),
 		ST_Y(geometrie),
-		fantoir,
+		'',
 		voie_cadastre,
+		fantoir,
 		ld_bati
 FROM	(SELECT	p.insee_com,
 				p.voie_cadastre,
