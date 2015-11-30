eb84c427245049e0a067a539ca1e877d51846c6a
vdct <vdct@laposte.net>
2015-11-30 22:14:59 +0100

    Lieux-dits : ajout au cycle quotidien (#50)

--- daily_bano.sh
+++ daily_bano.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 ./load_code_cadastre.sh
 ./import_echoues_france.py
-./load_cumul.py france osm
-./load_cumul.py france cadastre
+#./import_qadastre_france.py
+cat deplist.txt | parallel -j 5 -l 8 export LANG=fr_FR.UTF-8\; python load_cumul.py {1} OSM > /dev/null
+cat deplist.txt | parallel -j 5 -l 8 export LANG=fr_FR.UTF-8\; python load_cumul.py {1} CADASTRE > /dev/null
+cat deplist.txt | parallel -j 5 -l 8 export LANG=fr_FR.UTF-8\; python load_cumul_place.py {1} > /dev/null
+cd out
+./banout-all.sh
 
