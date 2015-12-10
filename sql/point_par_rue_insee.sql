d6f9e7b36957cec4d6769c00d7cbd78172427861
vdct <vdct@laposte.net>
2015-12-10 23:47:31 +0100

    Favoriser les voies incluses dans la commune (#69)

--- sql/point_par_rue_insee.sql
+++ sql/point_par_rue_insee.sql
@@ -1,30 +1,46 @@
+WITH w
+AS (SELECT	l.way,
+		l.name,
+		p.tags->'ref:INSEE' insee,
+		ST_Within(l.way,p.way)::integer within
+	FROM	planet_osm_polygon p
+	JOIN	planet_osm_line l
+	ON	ST_Intersects(l.way, p.way)
+	WHERE	p.tags ? 'ref:INSEE'		AND
+		p.tags->'ref:INSEE'='__com__'	AND
+		(l.highway IS NOT NULL		OR
+		l.waterway = 'dam')		AND
+		l.name IS NOT NULL),
+w1
+AS
+(SELECT	w.*,rank() OVER(PARTITION BY name ORDER BY within DESC) rang
+FROM	w),
+wl
+AS
+(SELECT ST_LineMerge(st_collect(way)) way,
+		name,
+		insee
+	FROM	w1
+	WHERE	rang = 1
+	GROUP BY 2,3),
+wp
+AS
+(SELECT ST_Centroid(ST_LineMerge(ST_Collect(way))) way,
+		name
+	FROM	w1
+	WHERE 	rang = 1
+	GROUP BY 2),
+a
+AS
+(SELECT	ST_Transform(ST_SetSRID(ST_ClosestPoint(wl.way,wp.way),900913),4326) point,
+	wl.name,
+	wl.insee
+FROM	wl
+JOIN	wp
+ON	wl.name = wp.name) 
+
 SELECT	ST_x(point),
 		ST_y(point),
 		name,
 		insee
-FROM (	WITH w
-		AS (SELECT	l.way,
-					l.name,
-					p.tags->'ref:INSEE' insee
-			FROM	planet_osm_polygon p
-			JOIN	planet_osm_line l
-			ON		ST_Intersects(l.way, p.way)
-			WHERE	p.tags ? 'ref:INSEE'			AND
-					p.tags->'ref:INSEE'='__com__'	AND
-					(l.highway IS NOT NULL			OR
-					l.waterway = 'dam')				AND
-					l.name IS NOT NULL)
-		SELECT	ST_Transform(ST_SetSRID(ST_ClosestPoint(wl.way,wp.way),900913),4326) point,
-				wl.name,
-				wl.insee
-		FROM	(SELECT ST_LineMerge(st_collect(way)) way,
-						name,
-						insee
-				FROM	w
-				GROUP BY 2,3) wl
-		JOIN	(SELECT ST_Centroid(ST_LineMerge(ST_Collect(way))) way,
-						name
-				FROM	w
-				GROUP BY 2) wp
-		ON		wl.name = wp.name) a;
-
+FROM a
