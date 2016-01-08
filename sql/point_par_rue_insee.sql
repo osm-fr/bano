870b05d98a5c515c708562fd76ea7f4105baeaf9
vdct <vdct@laposte.net>
2016-01-09 00:09:36 +0100

    Alt_name et old_name - voies sans adresses (#27)

--- sql/point_par_rue_insee.sql
+++ sql/point_par_rue_insee.sql
@@ -1,6 +1,6 @@
-WITH w
+WITH w0
 AS (SELECT	l.way,
-		l.name,
+		unnest(array[l.name,l.tags->'alt_name',l.tags->'old_name']) as name,
 		p.tags->'ref:INSEE' insee,
 		ST_Within(l.way,p.way)::integer within
 	FROM	planet_osm_polygon p
@@ -13,34 +13,39 @@ AS (SELECT	l.way,
 		l.name IS NOT NULL),
 w1
 AS
-(SELECT	w.*,rank() OVER(PARTITION BY name,insee ORDER BY within DESC) rang
-FROM	w),
+(SELECT *
+FROM 	w0
+WHERE 	name IS NOT NULL),
+w2
+AS
+(SELECT	*,rank() OVER(PARTITION BY name,insee ORDER BY within DESC) rang
+FROM	w1),
 wl
 AS
 (SELECT ST_LineMerge(st_collect(way)) way,
 		name,
 		insee
-	FROM	w1
+	FROM	w2
 	WHERE	rang = 1
 	GROUP BY 2,3),
 wp
 AS
 (SELECT ST_Centroid(ST_LineMerge(ST_Collect(way))) way,
 		name
-	FROM	w1
+	FROM	w2
 	WHERE 	rang = 1
 	GROUP BY 2),
 a
 AS
 (SELECT	ST_Transform(ST_SetSRID(ST_ClosestPoint(wl.way,wp.way),900913),4326) point,
-	wl.name,
-	wl.insee
+		wl.name,
+		wl.insee
 FROM	wl
 JOIN	wp
-ON	wl.name = wp.name) 
+ON		wl.name = wp.name) 
 
 SELECT	ST_x(point),
 		ST_y(point),
 		name,
 		insee
-FROM a
+FROM 	a;
