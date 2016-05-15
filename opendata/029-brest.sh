02b82fb3a1be4217c80327d88f1325b3eeac8d2c
cquest <cquest@openstreetmap.fr>
2016-05-15 19:15:44 +0200

    màj pour nouveaux fichiers et structure contenu

--- opendata/029-brest.sh
+++ opendata/029-brest.sh
@@ -1,12 +1,16 @@
 cd 029_brest
-rm -rf BMO_BaseAdresses*
+rm -rf REF_VOI*
 
-# récupération des shapefile contenant les points adresse
-curl https://geo.pays-de-brest.fr/donnees/Documents/Public/BMO_BaseAdresses.zip > BMO_BaseAdresses.zip
-unzip -o BMO_BaseAdresses.zip
+wget "https://applications002.brest-metropole.fr/VIPDU72/GPB/REF_VOI_ADRESSES_V.zip"
+unzip -o REF_VOI_ADRESSES_V.zip
 
 # import dans postGIS
-ogr2ogr -t_srs EPSG:4326 -f PostgreSQL PG:dbname=cadastre BMO_BaseAdresses/BMO_BaseAdresses/adre_p.shp -overwrite -nlt GEOMETRY -nln import_brest
+ogr2ogr -t_srs EPSG:4326 -f PostgreSQL PG:dbname=cadastre REF_VOI_ADRESSES_V.shp -overwrite -nlt GEOMETRY -nln import_brest
 
 # création des adresses dans cumul_adresses avec données FANTOIR
-psql cadastre -c "begin; delete from cumul_adresses where source='OD-BREST'; insert into cumul_adresses (select wkb_geometry as geometrie, trim(noimm || ' ' || lsiim) as numero, libru as voie_cadastre, nmvoi as voie_osm, idrue||f.cle_rivoli as fantoir, depco as insee_com, '', '029', null,'OD-BREST',null,'' from import_brest join fantoir_voie f on (f.code_insee=depco and f.id_voie=codru)); COMMIT;"
+psql cadastre -c "
+begin;
+delete from cumul_adresses where source='OD-BREST';
+insert into cumul_adresses (select wkb_geometry as geometrie, trim(noimm || ' ' || coalesce(lsiim,'')) as numero, libru as voie_cadastre, libru as voie_osm, idrue||f.cle_rivoli as fantoir, depco as insee_com, '', '029', null,'OD-BREST',null,'' from import_brest join fantoir_voie f on (f.code_insee=depco and f.id_voie=codru));
+COMMIT;
+"
