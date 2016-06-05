bd7c729a650ad3dcb9123bc39428e766bba79f79
vdct <vdct@laposte.net>
2016-06-05 23:22:41 +0200

    Nouveau paramètre CSRF_TOKEN sur cadastre.gouv.fr

--- load_code_cadastre.py
+++ load_code_cadastre.py
@@ -7,7 +7,6 @@ import os
 import xml.etree.ElementTree as ET
 
 a_depts = ['2A','2B','976']
-#a_depts = ['2A','2B']
 for n in range(1,20)+range(21,96)+range(971,975):
         a_depts.append(str(n).rjust(2,'0'))
 a_0_depts = [a.rjust(3,'0') for a in a_depts]
@@ -15,11 +14,14 @@ f_output = open('code_cadastre.csv','wb')
 cookieJar = cookielib.CookieJar()
 opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cookieJar))
 request = urllib2.Request('http://www.cadastre.gouv.fr/scpc/rechercherPlan.do')
+response = opener.open(request)
+token = response.read().split('CSRF_TOKEN=')[1].split('"')[0]
+
 for i,d in enumerate(a_0_depts):
 	fname = 'dept_'+a_0_depts[i]+'.xml'
 	time.sleep(1)
 	opener.open(request)
-	request = urllib2.Request('http://www.cadastre.gouv.fr/scpc/listerCommune.do?codeDepartement='+d+'&libelle=&keepVolatileSession=&offset=5000')
+	request = urllib2.Request("http://www.cadastre.gouv.fr/scpc/listerCommune.do?CSRF_TOKEN={:s}&codeDepartement={:s}&libelle=&keepVolatileSession=&offset=5000".format(token,d))
 	response = opener.open(request)
 	rep = response.read()
 	fw = open(fname,'wb')
@@ -35,7 +37,6 @@ for i,d in enumerate(a_0_depts):
 			commune_cp = lsplit[0].split('td class="nom"')[1].split('feuille')[0].split('strong')[1]
 			nom_commune = commune_cp[1:-11]
 			cp = commune_cp[-9:-4]
-			# print(code_commune,format,nom_commune,cp)
 			f_output.write('{:s},{:s},{:s},{:s},{:s},{:s}\n'.format(a_depts[i],d,nom_commune,cp,code_commune,format))
 		f_output.flush()
 	fr.close()
