24d1246799234b0413a20d644954475d8ff99668
vdct <vdct@laposte.net>
2018-12-01 12:04:37 +0100

    Suppression de la màj quotidienne de la source Cadastre

--- daily_bano.sh
+++ daily_bano.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 ./load_code_cadastre.sh
-./import_echoues_france.py
-./convert_qadastre_2_osm_differentiel.py
-./import_parcelles_buildings_differentiel.py
 cat deplist.txt | parallel -j 5 -l 8 export LANG=fr_FR.UTF-8\; python load_cumul.py {1} OSM > /dev/null
 cat deplist.txt | parallel -j 5 -l 8 export LANG=fr_FR.UTF-8\; python load_cumul.py {1} CADASTRE > /dev/null
 cat deplist.txt | parallel -j 5 -l 8 export LANG=fr_FR.UTF-8\; python load_cumul_place.py {1} > /dev/null
