9635d3c125dee0da2c8972bc94061f21aa02e873
Vincent de Château-Thierry <vdct@laposte.net>
2019-01-13 22:41:28 +0000

    Fantoir millésime octobre 2018

--- load_fantoir.sh
+++ load_fantoir.sh
@@ -3,7 +3,7 @@ cd $DATA_DIR
 mkdir fantoir
 cd fantoir
 rm -rf FANTOIR*
-wget https://static.data.gouv.fr/resources/fichier-fantoir-des-voies-et-lieux-dits/20180806-094626/FANTOIR0718.zip -O fantoir.zip
+wget https://static.data.gouv.fr/resources/fichier-fantoir-des-voies-et-lieux-dits/20181107-134510/fantoir1018.zip -O fantoir.zip
 unzip fantoir.zip
 mv FANTOIR* fantoir.txt
-psql -d cadastre -f $BANO_DIR/sql/load_fantoir.sql
+psql -d cadastre -U cadastre -f $BANO_DIR/sql/load_fantoir.sql
