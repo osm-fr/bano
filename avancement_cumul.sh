dd4eeb76e9e2c9997ba5ace0e05c1f830d3352b3
Vincent de Château-Thierry <vdct@laposte.net>
2019-01-29 07:09:57 +0000

    Outillage

--- avancement_cumul.sh
+++ avancement_cumul.sh
@@ -1,4 +1,4 @@
-psql -d cadastre -c "SELECT  source,etape, date_debut,date_fin,dept,cadastre_com,nom_com,nombre_adresses FROM batch ORDER BY id_batch DESC LIMIT 10;"
+psql -d cadastre -U cadastre -c "SELECT  source,etape, date_debut,date_fin,dept,cadastre_com,nom_com,nombre_adresses FROM batch ORDER BY id_batch DESC LIMIT 10;"
 ps -eaf|grep 'bash ./import-bano'
 ps -eaf|grep cumul
 ps -eaf|grep addr_2
