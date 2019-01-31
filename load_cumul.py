99f73372173268f9ca1602cb9094bdf4904d9de1
Vincent de Chateau-Thierry <vdechateauthierry@tp.michelin.com>
2019-01-31 17:30:14 +0100

    Mise en cache pour les couches avec suffixe (WIP)

--- load_cumul.py
+++ load_cumul.py
@@ -23,18 +23,36 @@ def get_code_dept_from_insee(insee):
     if insee[0:2] == '97':
         code_dept = insee[0:3]
     return code_dept
-def get_data_by_dept_from_pg(query_name,dept):
+def get_sql_like_dept_string(dept):
+    return (dept+'___')[0:5]
+def get_geom_suffixes(dept):
+    fq = open('sql/geom_suffixes_insee.sql','r')
+    str_query = fq.read().replace("='__com__"," LIKE  '{:s}'".format(get_sql_like_dept_string(dept)))
+    print(str_query)
+    cursor_bano_ro = pgc.cursor()
+    cursor_bano_ro.execute(str_query)
+    a_queries = []
+    for l in cursor_bano_ro :
+        a_queries.append("SELECT ST_PolygonFromText('{:s}',3857) as geom,'{:s}'::text suffixe".format(l[0],l[1].replace('\'','\'\'')))
+    cursor_bano_ro.close()
+    print(" UNION '.join(a_queries)")
+    print(' UNION '.join(a_queries))
+    return ' UNION '.join(a_queries)
+def warning_message_no_suffixe(dept,etape):
+    print("Pas de commune à suffixe dans le {}. Etape {} ignorée".format(dept,etape.upper()))
+
+def get_data_by_dept_from_pg(query_name,dept,suffixe_data=None):
     current_time = round(time.time())
-    if dept[0:1] == '0':
-        dept = dept[1:]
     etape_dept = 'cache_dept_'+query_name
 # Cache gardé 1h
     if o.age_etape_dept(etape_dept,dept) > 3600 :
         print(u'Mise à jour du cache "{:s}"'.format(query_name.upper()))
         batch_id = o.batch_start_log(source,etape_dept,dept)
         fq = open('sql/{:s}.sql'.format(query_name),'r')
-        str_sql_dept_like = (dept+'___')[0:5]
-        str_query = fq.read().replace('=\'__com__',' LIKE  \'{:s}'.format(str_sql_dept_like))
+        str_query = fq.read().replace('=\'__com__',' LIKE  \'{:s}'.format(get_sql_like_dept_string(dept)))
+        if suffixe_data :
+            str_query = str_query.replace('__suffixe_data__',suffixe_data)
+            print(str_query)            
         fq.close()
         cur_osm_ro = pgcl.cursor()
         cur_osm_ro.execute(str_query)
@@ -46,32 +64,24 @@ def get_data_by_dept_from_pg(query_name,dept):
                 if item == None:
                     list_values.append('null')
                 elif  type(item) == str :
-                    # list_values.append("'{}'".format(item.replace("'","''")))
                     list_values.append("'{}'".format(item.replace("'","''").replace('"','')))
                 elif type(item) == list :
                     if (len(item)) > 0 :
-                        # list_values.append("hstore(ARRAY{})".format(str([s.replace("'","''") for s in item])))
                         list_values.append("hstore(ARRAY{})".format(str([s.replace("'","''").replace('"','') for s in item])))
                     else :
                         list_values.append('null')
                 else :
                     list_values.append(str(item))
             list_values.append(str(current_time))
-            # print(list_values)
 
             str_values = ','.join(list_values).replace('"',"'")
-            # str_values = str_values.replace("'","''").replace('"',"'").replace("'',''","','").replace("'', ''","','")
-            # str_values = str_values.replace('"',"'")
-            # print(str_values)
             list_output.append(str_values)
         cur_osm_ro.close()
-        # pgc_bano_rw = get_pgc()
         cur_bano_rw = pgc.cursor()
-        str_query = "DELETE FROM {} WHERE insee_com LIKE '{}';".format(query_name,str_sql_dept_like)
+        str_query = "DELETE FROM {} WHERE insee_com LIKE '{}';".format(query_name,get_sql_like_dept_string(dept))
         cur_bano_rw.execute(str_query)
         if len(list_output) > 0 :
             str_query = "INSERT INTO {} VALUES ({});COMMIT;".format(query_name,'),('.join(list_output))
-            # print(str_query)
             cur_bano_rw.execute(str_query)
 
         o.batch_end_log(0,batch_id)
@@ -95,10 +105,10 @@ pgc = a.get_pgc()
 pgcl = a.get_pgc_layers()
 
 if sys.argv[1].upper() == 'FRANCE':
-    loop_query = 'SELECT DISTINCT cadastre_dept FROM code_cadastre ORDER BY 1;'
+    loop_query = 'SELECT DISTINCT dept FROM code_cadastre ORDER BY 1;'
 else:
-    num_dept_cadastre = ('000'+sys.argv[1])[-3:]
-    loop_query = 'SELECT \'{:s}\';'.format(num_dept_cadastre)
+    num_dept_cadastre = sys.argv[1]
+    loop_query = "SELECT '{:s}';".format(num_dept_cadastre)
 
 cur_loop = pgc.cursor()
 cur_loop.execute(loop_query)
@@ -107,8 +117,14 @@ for c_loop in cur_loop:
     global f_log
     f_log = e.start_log_to_file(source,os.path.basename(sys.argv[0]).split('.')[0],num_dept_cadastre)
     print('## Département {:s}'.format(num_dept_cadastre))
+    geom_suffixe = get_geom_suffixes(num_dept_cadastre)
+    print(geom_suffixe)
     if source == 'OSM':
         get_data_by_dept_from_pg('hsnr_insee',num_dept_cadastre)
+        if geom_suffixe :
+            get_data_by_dept_from_pg('hsnr_suffixe_insee',num_dept_cadastre,geom_suffixe)
+        else :
+            warning_message_no_suffixe(num_dept_cadastre,'hsnr_suffixe_insee')
         get_data_by_dept_from_pg('hsnr_bbox_insee',num_dept_cadastre)
         get_data_by_dept_from_pg('point_par_rue_insee',num_dept_cadastre)
         get_data_by_dept_from_pg('point_par_rue_complement_insee',num_dept_cadastre)
@@ -116,8 +132,16 @@ for c_loop in cur_loop:
     # os._exit(0)
     
     get_data_by_dept_from_pg('highway_insee',num_dept_cadastre)
+    if geom_suffixe :
+        get_data_by_dept_from_pg('highway_suffixe_insee',num_dept_cadastre,geom_suffixe)
+    else :
+        warning_message_no_suffixe(num_dept_cadastre,'highway_suffixe_insee')
     get_data_by_dept_from_pg('highway_bbox_insee',num_dept_cadastre)
     get_data_by_dept_from_pg('highway_relation_insee',num_dept_cadastre)
+    if geom_suffixe :
+        get_data_by_dept_from_pg('highway_relation_suffixe_insee',num_dept_cadastre,geom_suffixe)
+    else :
+        warning_message_no_suffixe(num_dept_cadastre,'highway_relation_suffixe_insee')
     get_data_by_dept_from_pg('highway_relation_bbox_insee',num_dept_cadastre)
     # os._exit(0)
 
