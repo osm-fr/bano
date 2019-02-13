36ebcb2b915ff6b9604cd1e6f54e16cd242a62aa
Vincent de Château-Thierry <vdct@laposte.net>
2019-02-13 21:26:54 +0000

    Tables de cache coté OSM & index

--- load_osm_france_db.sh
+++ load_osm_france_db.sh
@@ -1,3 +1,5 @@
 # wget http://download.openstreetmap.fr/extracts/merge/france_metro_dom_com_nc.osm.pbf
 /app/imposm/imposm import -mapping ./bano.yml -read /data/download/france_metro_dom_com_nc.osm.pbf -overwritecache -cachedir /data/bano_imposm_cachedir -dbschema-import public -diff -diffdir /data/bano_imposm_diffdir
 /app/imposm/imposm import -mapping ./bano.yml -write -connection 'postgis://cadastre@localhost/osm'?prefix=NONE -cachedir /data/bano_imposm_cachedir -dbschema-import public -diff -diffdir /data/bano_imposm_diffdir
+
+psql -d osm -U cadastre -f sql/finalisation.sql
