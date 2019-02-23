62a8d8252ad166022664731d4424652109fdf48d
Vincent de Château-Thierry <vdct@laposte.net>
2019-02-23 22:30:42 +0000

    Fantoir : millesime 0119 et base osm

--- load_fantoir.sh
+++ load_fantoir.sh
@@ -3,7 +3,7 @@ cd $DATA_DIR
 mkdir fantoir
 cd fantoir
 rm -rf FANTOIR*
-wget https://static.data.gouv.fr/resources/fichier-fantoir-des-voies-et-lieux-dits/20181107-134510/fantoir1018.zip -O fantoir.zip
+wget https://static.data.gouv.fr/resources/fichier-fantoir-des-voies-et-lieux-dits/20190205-142621/fantoir0119.zip -O fantoir.zip
 unzip fantoir.zip
 mv FANTOIR* fantoir.txt
-psql -d cadastre -U cadastre -f $BANO_DIR/sql/load_fantoir.sql
+psql -d osm -U cadastre -f $BANO_DIR/sql/load_fantoir.sql
