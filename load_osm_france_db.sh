9b8d66fbcb58504146744c4f6879bf418dbea7b2
Vincent de Château-Thierry <vdct@laposte.net>
2019-04-14 21:34:48 +0000

    fix sur le chargement

--- load_osm_france_db.sh
+++ load_osm_france_db.sh
@@ -1,5 +1,8 @@
-# wget http://download.openstreetmap.fr/extracts/merge/france_metro_dom_com_nc.osm.pbf
+source config
+cd /data/download
+rm -rf france_metro_dom_com_nc.osm.pbf
+wget http://download.openstreetmap.fr/extracts/merge/france_metro_dom_com_nc.osm.pbf
 /app/imposm/imposm import -mapping ./bano.yml -read /data/download/france_metro_dom_com_nc.osm.pbf -overwritecache -cachedir /data/bano_imposm_cachedir -dbschema-import public -diff -diffdir /data/bano_imposm_diffdir
 /app/imposm/imposm import -mapping ./bano.yml -write -connection 'postgis://cadastre@localhost/osm'?prefix=NONE -cachedir /data/bano_imposm_cachedir -dbschema-import public -diff -diffdir /data/bano_imposm_diffdir
 
-psql -d osm -U cadastre -f sql/finalisation.sql
+psql -d osm -U cadastre -f $BANO_DIR/sql/finalisation.sql
