d32069811b2bfdb8bbfad05cb1797ff264f4acfc
Vincent de Chateau-Thierry <vdct@laposte.net>
2019-05-10 18:37:24 +0200

    variables

--- load_osm_france_db.sh
+++ load_osm_france_db.sh
@@ -2,7 +2,7 @@ source config
 cd /data/download
 rm -rf france_metro_dom_com_nc.osm.pbf
 wget http://download.openstreetmap.fr/extracts/merge/france_metro_dom_com_nc.osm.pbf
-/app/imposm/imposm import -mapping ./bano.yml -read /data/download/france_metro_dom_com_nc.osm.pbf -overwritecache -cachedir /data/bano_imposm_cachedir -dbschema-import public -diff -diffdir /data/bano_imposm_diffdir
-/app/imposm/imposm import -mapping ./bano.yml -write -connection 'postgis://cadastre@localhost/osm'?prefix=NONE -cachedir /data/bano_imposm_cachedir -dbschema-import public -diff -diffdir /data/bano_imposm_diffdir
+/app/imposm/imposm import -mapping $BANO_DIR/bano.yml -read /data/download/france_metro_dom_com_nc.osm.pbf -overwritecache -cachedir /data/bano_imposm_cache -dbschema-import public -diff -diffdir /data/bano_imposm_diff
+/app/imposm/imposm import -mapping $BANO_DIR/bano.yml -write -connection 'postgis://cadastre@localhost/osm'?prefix=NONE -cachedir /data/bano_imposm_cache -dbschema-import public -diff -diffdir /data/bano_imposm_diff
 
 psql -d osm -U cadastre -f $BANO_DIR/sql/finalisation.sql
