2595d7517995c0ee3f05e9ae7b5e2449caf5ccc0
vdct <vdct@laposte.net>
2019-05-20 00:20:00 +0200

    Nouveau subparser update_code_cadastre

--- bano/bin.py
+++ bano/bin.py
@@ -3,7 +3,7 @@ import sys
 
 from .constants import DEPARTEMENTS
 from .core import addr_2_db
-from .sources import bal
+from .sources import bal, cadastre_gouv
 
 
 def main():
@@ -13,10 +13,12 @@ def main():
     subparser.add_argument('source', choices=['OSM', 'CADASTRE', 'BAL'], type=str, help='Source des données à traiter')
     subparser.add_argument('code_insee', type=str, help='Code INSEE de la commune à traiter')
     subparser.set_defaults(func=addr_2_db)
-    subparser = subparsers.add_parser('download_bal', help='b help', description="Télécharge un fichier d'adresses")
+    subparser = subparsers.add_parser('download_bal', help='b help', description="Met à jour les fichiers d'adresses au format BAL")
     subparser.add_argument('source', choices=['CADASTRE', 'BAL'], type=str, help='Source des données à traiter')
     subparser.add_argument('departements', type=str, help='Départements à traiter', nargs='*', default=DEPARTEMENTS)
     subparser.set_defaults(func=bal.process)
+    subparser = subparsers.add_parser('update_code_cadastre', help='b help', description="Met à jour la liste des communes d'après cadastre.gouv.fr")
+    subparser.set_defaults(func=cadastre_gouv.process)
 
     args = parser.parse_args()
 
@@ -24,5 +26,6 @@ def main():
         args.func(**vars(args))
     except ValueError as err:
         sys.exit(str(err))
-
+    except TypeError as err:
+        import ipdb; ipdb.set_trace()
 
