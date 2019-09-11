ab232bcbe0840201f33206e8cd7613648c0157e1
Vincent de Château-Thierry <vdct@laposte.net>
2019-09-11 20:23:34 +0000

    Chargement initial des données OSM

--- load_osm_france_db.sh
+++ load_osm_france_db.sh
@@ -1,8 +1,11 @@
-source config
+#!/bin/bash
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+source $SCRIPT_DIR/config
+
 cd /data/download
-rm -rf france_metro_dom_com_nc.osm.pbf
-wget http://download.openstreetmap.fr/extracts/merge/france_metro_dom_com_nc.osm.pbf
-/app/imposm/imposm import -mapping $BANO_DIR/bano.yml -read /data/download/france_metro_dom_com_nc.osm.pbf -overwritecache -cachedir /data/bano_imposm_cache -dbschema-import public -diff -diffdir /data/bano_imposm_diff
-/app/imposm/imposm import -mapping $BANO_DIR/bano.yml -write -connection 'postgis://cadastre@localhost/osm'?prefix=NONE -cachedir /data/bano_imposm_cache -dbschema-import public -diff -diffdir /data/bano_imposm_diff
+wget -NS http://download.openstreetmap.fr/extracts/merge/france_metro_dom_com_nc.osm.pbf
+imposm import -mapping $BANO_DIR/bano.yml -read /data/download/france_metro_dom_com_nc.osm.pbf -overwritecache -cachedir /data/bano_imposm_cache -dbschema-import public -diff -diffdir /data/bano_imposm_diff
+imposm import -mapping $BANO_DIR/bano.yml -write -connection 'postgis://cadastre@localhost/osm'?prefix=NONE -cachedir /data/bano_imposm_cache -dbschema-import public -diff -diffdir /data/bano_imposm_diff
 
 psql -d osm -U cadastre -f $BANO_DIR/sql/finalisation.sql
