de5d05026e8083aaf1acbb846e9b65ebf438f1bd
Vincent de Château-Thierry <vdct@laposte.net>
2019-09-23 04:34:40 +0000

    Fantoir juillet 2019

--- load_fantoir.sh
+++ load_fantoir.sh
@@ -1,9 +1,11 @@
+#!/bin/bash
+
 source config
 cd $DATA_DIR
-mkdir fantoir
+mkdir -p fantoir
 cd fantoir
-rm -rf FANTOIR*
-wget https://static.data.gouv.fr/resources/fichier-fantoir-des-voies-et-lieux-dits/20190205-142621/fantoir0119.zip -O fantoir.zip
+rm -rf FANTOIR* && rm *.zip
+wget https://data.economie.gouv.fr/api/datasets/1.0/fichier-fantoir-des-voies-et-lieux-dits/attachments/fichier_national_fantoir_situation_juillet_2019_zip -O fantoir.zip
 unzip fantoir.zip
 mv FANTOIR* fantoir.txt
 psql -d osm -U cadastre -f $BANO_DIR/sql/load_fantoir.sql
