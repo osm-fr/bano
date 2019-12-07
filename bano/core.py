c6a6d4ad28cd519452d0950d11c1d36efaef7433
Vincent de Château-Thierry <vdct@laposte.net>
2019-12-07 22:12:31 +0000

    suffixes invalides dans la source OSM (#163)

--- bano/core.py
+++ bano/core.py
@@ -117,7 +117,7 @@ def load_bases_adresses_locales_hsnr(code_insee):
 def load_hsnr_bbox_from_pg_osm(insee_com):
     data = get_data_from_pg_direct('hsnr_bbox_insee',insee_com)
     for x, y, provenance, osm_id, numero, voie, tags, *others in data:
-        for num in numero.translate(str.maketrans(',à;-/','$$$$$')).split('$'):
+        for num in numero.translate(str.maketrans(',à;-/*','$$$$$$')).split('$'):
             oa = Pg_hsnr([x, y, provenance, osm_id, num.rstrip().lstrip(), voie, tags], insee_com)
             if oa.fantoir == '' or not oa.voie :
                 continue
@@ -127,7 +127,7 @@ def load_hsnr_bbox_from_pg_osm(insee_com):
 def load_hsnr_from_pg_osm(insee_com):
     data = get_data_from_pg_direct('hsnr_insee', insee_com)
     for x, y, provenance, osm_id, numero, voie, tags, *others in data:
-        for num in numero.translate(str.maketrans(',à;-/','$$$$$')).split('$'):
+        for num in numero.translate(str.maketrans(',à;-/*','$$$$$$')).split('$'):
             oa = Pg_hsnr([x, y, provenance, osm_id, num.rstrip().lstrip(), voie, tags], insee_com)
             if not oa.voie :
                 continue
