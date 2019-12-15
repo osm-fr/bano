493635806cb1e381321a3371f6f7af1bec4d32b2
Vincent de Château-Thierry <vdct@laposte.net>
2019-12-15 08:44:30 +0000

    Nouveaux index pour osm-vs-fantoir #71

--- sql/finalisation.sql
+++ sql/finalisation.sql
@@ -1,2 +1,5 @@
+CREATE INDEX idx_planet_osm_point_fantoir ON planet_osm_point("ref:FR:FANTOIR");
+CREATE INDEX idx_planet_osm_line_fantoir ON planet_osm_line("ref:FR:FANTOIR");
+CREATE INDEX idx_planet_osm_polygon_fantoir ON planet_osm_polygon("ref:FR:FANTOIR");
 CREATE INDEX idx_planet_osm_polygon_ref_insee ON planet_osm_polygon("ref:INSEE");
 CREATE INDEX idx_planet_osm_rels_id ON planet_osm_rels(osm_id);
