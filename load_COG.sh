36adae34230091b0723a5e1ddb6240d6ccb6c138
Vincent de Château-Thierry <vdct@laposte.net>
2020-01-04 22:21:21 +0000

    COG : chargement 2019 - regions (#155) pour les exports (#166)

--- load_COG.sh
+++ load_COG.sh
@@ -4,17 +4,20 @@ source config
 cd $DATA_DIR
 mkdir -p cog
 cd cog
-rm *.csv && rm *.zip
+rm *.csv && rm *2019.zip
 wget -NS https://www.insee.fr/fr/statistiques/fichier/3720946/cog_ensemble_2019_csv.zip
 unzip cog_ensemble_2019_csv.zip
 
 CSV_COMMUNE=communes-01042019.csv
+CSV_CANTON=canton2019.csv
 CSV_DEPARTEMENT=departement2019.csv
+CSV_REGION=region2019.csv
 
-psql -d osm -U cadastre -f $BANO_DIR/sql/create_table_cog.sql
-cat ${CSV_COMMUNE}|psql -d osm -U cadastre -c "TRUNCATE TABLE cog_commune;COPY cog_commune FROM stdin WITH csv header delimiter ',';"
-cat ${CSV_DEPARTEMENT}|psql -d osm -U cadastre -c "TRUNCATE TABLE cog_departement;COPY cog_departement FROM stdin WITH csv header delimiter ',';"
-
-psql -d cadastre -U cadastre -f $BANO_DIR/sql/create_table_cog.sql
-cat ${CSV_COMMUNE}|psql -d cadastre -U cadastre -c "TRUNCATE TABLE cog_commune;COPY cog_commune FROM stdin WITH csv header delimiter ',';"
-cat ${CSV_DEPARTEMENT}|psql -d cadastre -U cadastre -c "TRUNCATE TABLE cog_departement;COPY cog_departement FROM stdin WITH csv header delimiter ',';"
+for d in osm cadastre
+do
+  psql -d ${d} -U cadastre -f $BANO_DIR/sql/create_table_cog.sql
+  cat ${CSV_COMMUNE}|psql -d ${d} -U cadastre -c "TRUNCATE TABLE cog_commune;COPY cog_commune FROM stdin WITH csv header delimiter ',';"
+  cat ${CSV_CANTON}|psql -d ${d} -U cadastre -c "TRUNCATE TABLE cog_canton;COPY cog_canton FROM stdin WITH csv header delimiter ',';"
+  cat ${CSV_DEPARTEMENT}|psql -d ${d} -U cadastre -c "TRUNCATE TABLE cog_departement;COPY cog_departement FROM stdin WITH csv header delimiter ',';"
+  cat ${CSV_REGION}|psql -d ${d} -U cadastre -c "TRUNCATE TABLE cog_region;COPY cog_region FROM stdin WITH csv header delimiter ',';"
+done
--- sql/create_table_cog.sql
+++ sql/create_table_cog.sql
@@ -10,9 +10,20 @@ CREATE TABLE IF NOT EXISTS cog_commune (
         libelle text,
         can character(5),
         comparent character(5));
-
-
 CREATE INDEX IF NOT EXISTS idx_cog_commune_com ON cog_commune(com);
+
+CREATE TABLE IF NOT EXISTS cog_canton (
+        can character(5),
+        dep character varying(3),
+        reg character(2),
+        compct character(1),
+        burcentral character(5),
+        tncc character(1),
+        ncc text,
+        nccenr text,
+        libelle text,
+        typect character(1));
+CREATE INDEX IF NOT EXISTS idx_cog_canton_can ON cog_canton(can);
 
 CREATE TABLE IF NOT EXISTS cog_departement (
         dep character varying(3),
@@ -22,6 +33,13 @@ CREATE TABLE IF NOT EXISTS cog_departement (
         ncc text,
         nccenr text,
         libelle text);
-
-
 CREATE INDEX IF NOT EXISTS idx_cog_departement_dep ON cog_departement(dep);
+
+CREATE TABLE IF NOT EXISTS cog_region (
+        reg character(2),
+        cheflieu character(5),
+        tncc character(1),
+        ncc text,
+        nccenr text,
+        libelle text);
+CREATE INDEX IF NOT EXISTS idx_cog_region_reg ON cog_region(reg);
