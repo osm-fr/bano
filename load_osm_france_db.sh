df12d43b3fb94e3743b75312c8f800f6f877d35f
Vincent de Château-Thierry <vdct@laposte.net>
2020-05-17 13:24:55 +0000

    récuperation du state.tx d'imposm à l'import

--- load_osm_france_db.sh
+++ load_osm_france_db.sh
@@ -9,3 +9,5 @@ imposm import -mapping $BANO_DIR/bano.yml -read /data/download/france_metro_dom_
 imposm import -mapping $BANO_DIR/bano.yml -write -connection 'postgis://cadastre@localhost/osm'?prefix=NONE -cachedir /data/bano_imposm_cache -dbschema-import public -diff -diffdir /data/bano_imposm_diff
 
 psql -d osm -U cadastre -f $BANO_DIR/sql/finalisation.sql
+
+cp /data/bano_imposm_diff/last.state.txt /data/download/state.txt
