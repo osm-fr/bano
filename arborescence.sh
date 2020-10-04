c5b7ec0533c2d46e173eb4566b369ca61830a55e
Vincent de Château-Thierry <vdct@laposte.net>
2020-10-04 05:36:25 +0000

    Arborescence BAN ODbL (#196)

--- arborescence.sh
+++ arborescence.sh
@@ -4,6 +4,9 @@ source config
 mkdir $LOG_DIR
 mkdir $DATA_DIR
 mkdir $BANO_CACHE_DIR
+mkdir $BAN_CACHE_DIR
 mkdir $CADASTRE_CACHE_DIR
 mkdir $BAL_CACHE_DIR
 mkdir $EXPIRE_TILES_DIR
+mkdir -p $EXPORT_SAS_DIR
+mkdir -p $EXPORT_WEB_DIR
