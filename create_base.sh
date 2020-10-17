751d5a6e3588fb9b37b53565ca27abc7dca6c01b
Vincent de Château-Thierry <vdct@laposte.net>
2020-10-17 16:39:24 +0000

    Reorganisation - setup des bases

--- create_base.sh
+++ create_base.sh
@@ -3,5 +3,6 @@ createuser -s -d cadastre
 createdb -E UTF-8 -T template0 -O cadastre cadastre
 exit
 psql -d cadastre -U cadastre -f sql/create_base.sql
-psql -d cadastre -U cadastre -f sql/create_table.sql -v schema_cible=public
-psql -d cadastre -U cadastre -f sql/create_table.sql -v schema_cible=dev
+psql -d cadastre -U cadastre -f sql/create_table_base_bano.sql
+psql -d osm -U cadastre -f sql/create_base.sql
+psql -d osm -U cadastre -f sql/create_table_base_osm.sql
