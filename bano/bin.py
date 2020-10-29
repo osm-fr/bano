0fcdf36d72d950a73cb0daf40fdc23fbe8f350cb
Vincent de Château-Thierry <vdct@laposte.net>
2020-10-29 21:16:45 +0000

    Description des sous-commandes de bano

--- bano/bin.py
+++ bano/bin.py
@@ -8,57 +8,155 @@ from .sources import bal, cadastre_gouv, cadastre_json, ban
 
 
 def main():
-    parser = argparse.ArgumentParser(prog='bano', description='Donne accès à tous les traitements')
-    subparsers = parser.add_subparsers(help='Commandes disponibles')
+    parser = argparse.ArgumentParser(
+        prog="bano", description="Donne accès à tous les traitements"
+    )
+    subparsers = parser.add_subparsers(help="Commandes disponibles")
 
-    subparser = subparsers.add_parser('process_commune', help='a help', description='Traite une commune')
-    subparser.add_argument('source', choices=['OSM', 'CADASTRE', 'BAL','BAN'], type=str, help='Source des données à traiter')
+    subparser = subparsers.add_parser(
+        "process_commune",
+        help="Traite unitairement les adresses d'une commune",
+        description="Traite unitairement les adresses d'une commune",
+    )
+    subparser.add_argument(
+        "source",
+        choices=["OSM", "CADASTRE", "BAL", "BAN"],
+        type=str,
+        help="Source des données à traiter",
+    )
     group = subparser.add_mutually_exclusive_group(required=True)
-    group.add_argument('--code_insee', type=str, help='Code INSEE de la commune à traiter')
-    group.add_argument('--depts', type=str, help='Départements à traiter', nargs='*')
-    group.add_argument('--France', action='store_const', const=DEPARTEMENTS)
+    group.add_argument(
+        "--code_insee", type=str, help="Code INSEE de la commune à traiter"
+    )
+    group.add_argument("--depts", type=str, help="Départements à traiter (toutes les communes de chaque dept sont traitées une par une)", nargs="*")
+    group.add_argument("--France", help="Raccourci pour tous les départements d'un coup", action="store_const", const=DEPARTEMENTS)
     subparser.set_defaults(func=core.process)
 
-    subparser = subparsers.add_parser('process_commune_lieux-dits', help='a help', description='Traite une commune')
+    subparser = subparsers.add_parser(
+        "process_commune_lieux-dits",
+        help="Traite unitairement les lieux-dits d'une commune", 
+        description="Traite unitairement les lieux-dits d'une commune"
+    )
     group = subparser.add_mutually_exclusive_group(required=True)
-    group.add_argument('--code_insee', type=str, help='Code INSEE de la commune à traiter')
-    group.add_argument('--depts', type=str, help='Départements à traiter', nargs='*')
-    group.add_argument('--France', action='store_const', const=DEPARTEMENTS)
+    group.add_argument(
+        "--code_insee", type=str, help="Code INSEE de la commune à traiter"
+    )
+    group.add_argument("--depts", type=str, help="Départements à traiter (toutes les communes de chaque dept sont traitées une par une)", nargs="*")
+    group.add_argument("--France", help="Raccourci pour tous les départements d'un coup", action="store_const", const=DEPARTEMENTS)
     subparser.set_defaults(func=core_place.process)
 
-    subparser = subparsers.add_parser('download_bal', help='b help', description="Met à jour les fichiers d'adresses au format BAL")
-    subparser.add_argument('source', choices=['CADASTRE', 'BAL'], type=str, help='Source des données à traiter')
-    subparser.add_argument('departements', type=str, help='Départements à traiter', nargs='*', default=DEPARTEMENTS)
+    subparser = subparsers.add_parser(
+        "download_bal",
+        help="Met à jour les fichiers d'adresses au format BAL",
+        description="Met à jour les fichiers d'adresses au format BAL",
+    )
+    subparser.add_argument(
+        "source",
+        choices=["CADASTRE", "BAL"],
+        type=str,
+        help="Source des données à traiter",
+    )
+    subparser.add_argument(
+        "departements",
+        type=str,
+        help="Départements à traiter",
+        nargs="*",
+        default=DEPARTEMENTS,
+    )
     subparser.set_defaults(func=bal.process)
-    
-    subparser = subparsers.add_parser('download_ban', help='b help', description="Met à jour les fichiers d'adresses de source BAN")
-    subparser.add_argument('departements', type=str, help='Départements à traiter', nargs='*', default=DEPARTEMENTS)
+
+    subparser = subparsers.add_parser(
+        "download_ban",
+        help="Met à jour les fichiers d'adresses de source BAN",
+        description="Met à jour les fichiers d'adresses de source BAN",
+    )
+    subparser.add_argument(
+        "departements",
+        type=str,
+        help="Départements à traiter",
+        nargs="*",
+        default=DEPARTEMENTS,
+    )
     subparser.set_defaults(func=ban.process)
 
-    subparser = subparsers.add_parser('download_cadastre', help='b help', description="Met à jour les fichiers du cadastre hors adresses au format JSON")
-    subparser.add_argument('prefixe', choices=['lieux_dits'], type=str, help='Source des données à traiter')
-    subparser.add_argument('departements', type=str, help='Départements à traiter', nargs='*', default=DEPARTEMENTS)
+    subparser = subparsers.add_parser(
+        "download_cadastre",
+        help="Met à jour les fichiers du cadastre hors adresses au format JSON",
+        description="Met à jour les fichiers du cadastre hors adresses au format JSON",
+    )
+    subparser.add_argument(
+        "prefixe", choices=["lieux_dits"], type=str, help="Source des données à traiter"
+    )
+    subparser.add_argument(
+        "departements",
+        type=str,
+        help="Départements à traiter",
+        nargs="*",
+        default=DEPARTEMENTS,
+    )
     subparser.set_defaults(func=cadastre_json.process)
 
-    subparser = subparsers.add_parser('update_code_cadastre', help='b help', description="Met à jour la liste des communes d'après cadastre.gouv.fr")
+    subparser = subparsers.add_parser(
+        "update_code_cadastre",
+        help="Met à jour la liste des communes d'après cadastre.gouv.fr - indique le format du Cadastre",
+        description="Met à jour la liste des communes d'après cadastre.gouv.fr - indique le format du Cadastre",
+    )
     subparser.set_defaults(func=cadastre_gouv.process)
-    
-    subparser = subparsers.add_parser('pre_process_suffixe', help='b help', description="Détermine les zones où les noms dans le Cadastre sont suffixés")
-    subparser.add_argument('departements', type=str, help='Départements à traiter', nargs='*', default=DEPARTEMENTS)
+
+    subparser = subparsers.add_parser(
+        "pre_process_suffixe",
+        help="Détermine les zones où les noms dans le Cadastre sont suffixés",
+        description="Détermine les zones où les noms dans le Cadastre sont suffixés",
+    )
+    subparser.add_argument(
+        "departements",
+        type=str,
+        help="Départements à traiter",
+        nargs="*",
+        default=DEPARTEMENTS,
+    )
     subparser.set_defaults(func=pre_process_suffixe.process)
 
-    subparser = subparsers.add_parser('export', help='b help', description="Export par département dans différents formats")
-    subparser.add_argument('departements', type=str, help='Départements à traiter', nargs='*', default=DEPARTEMENTS)
+    subparser = subparsers.add_parser(
+        "export",
+        help="Export par département dans différents formats",
+        description="Export par département dans différents formats",
+    )
+    subparser.add_argument(
+        "departements",
+        type=str,
+        help="Départements à traiter",
+        nargs="*",
+        default=DEPARTEMENTS,
+    )
     subparser.set_defaults(func=export.process)
 
-    subparser = subparsers.add_parser('publish', help='b help', description="Publication des exports dans un dossier web - fichiers par département")
-    subparser.add_argument('departements', type=str, help='Départements à traiter', nargs='*', default=DEPARTEMENTS)
+    subparser = subparsers.add_parser(
+        "publish",
+        help="Publication des exports dans le dossier web de https://bano.openstreetmap.fr/data - fichiers par département",
+        description="Publication des exports dans le dossier web de https://bano.openstreetmap.fr/data - fichiers par département",
+    )
+    subparser.add_argument(
+        "departements",
+        type=str,
+        help="Départements à traiter",
+        nargs="*",
+        default=DEPARTEMENTS,
+    )
     subparser.set_defaults(func=publish.process)
 
-    subparser = subparsers.add_parser('publish_aggregate', help='b help', description="Publication des exports dans un dossier web - fichiers France")
+    subparser = subparsers.add_parser(
+        "publish_aggregate",
+        help="Publication des exports dans un dossier web - fichiers France entière",
+        description="Publication des exports dans un dossier web - fichiers France entière",
+    )
     subparser.set_defaults(func=publish.process_full)
 
-    subparser = subparsers.add_parser('update_insee_lists', help='b help', description="Détermine les communes mises à jour grace aux tuiles impactées")
+    subparser = subparsers.add_parser(
+        "update_insee_lists",
+        help="Détermine les communes mises à jour grace aux tuiles impactées",
+        description="Détermine les communes mises à jour grace aux tuiles impactées",
+    )
     subparser.set_defaults(func=update_manager.update_insee_lists)
 
     args = parser.parse_args()
@@ -68,5 +166,6 @@ def main():
     except ValueError as err:
         sys.exit(str(err))
     except TypeError as err:
-        import ipdb; ipdb.set_trace()
+        import ipdb
 
+        ipdb.set_trace()
