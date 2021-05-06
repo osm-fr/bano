62d77a69045c1f4d67cb0dc75b1982d7d967f448
Vincent de Château-Thierry <vdct@laposte.net>
2021-05-06 16:24:23 +0000

    Fantoir avril 21 (#242)

--- load_fantoir.sh
+++ load_fantoir.sh
@@ -5,7 +5,7 @@ cd $DATA_DIR
 mkdir -p fantoir
 cd fantoir
 rm -rf FANTOIR* && rm *.zip
-wget https://data.economie.gouv.fr/api/datasets/1.0/fichier-fantoir-des-voies-et-lieux-dits/attachments/fichier_national_fantoir_situation_janvier_2021_zip -O fantoir.zip
+wget https://data.economie.gouv.fr/api/datasets/1.0/fichier-fantoir-des-voies-et-lieux-dits/attachments/fichier_national_fantoir_situation_avril_2021_zip -O fantoir.zip
 unzip fantoir.zip
 mv FANTOIR* fantoir.txt
 psql -d osm -U cadastre -f $BANO_DIR/sql/load_fantoir.sql
