f072ad476a2b2e5a04b64d938b8f34699571322a
Vincent de Château-Thierry <vdct@laposte.net>
2022-08-15 19:13:34 +0000

    Autocommit pour toutes les bases

--- bano/db.py
+++ bano/db.py
@@ -3,6 +3,8 @@ import os
 import psycopg2
 import psycopg2.extras
 
-# bano = psycopg2.connect(os.environ.get("BANO_PG", "dbname='cadastre' user='cadastre'"))
+bano = psycopg2.connect(os.environ.get("BANO_PG", "dbname='bano' user='cadastre'"))
+bano.autocommit = True
 bano_sources = psycopg2.connect(os.environ.get("BANO_PG_CACHE", "dbname='bano_sources' user='cadastre'"))
+bano_sources.autocommit = True
 # psycopg2.extras.register_hstore(bano_cache)
