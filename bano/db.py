b35ea3df1d17590633df05284c685bde302c6a49
Vincent de Château-Thierry <vdct@laposte.net>
2022-10-02 21:35:07 +0000

    PG hstore => Python dict

--- bano/db.py
+++ bano/db.py
@@ -7,4 +7,4 @@ bano = psycopg2.connect(os.environ.get("BANO_PG", "dbname='bano' user='cadastre'
 bano.autocommit = True
 bano_sources = psycopg2.connect(os.environ.get("BANO_PG_CACHE", "dbname='bano_sources' user='cadastre'"))
 bano_sources.autocommit = True
-# psycopg2.extras.register_hstore(bano_cache)
+psycopg2.extras.register_hstore(bano_sources)
