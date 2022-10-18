a5bfea1dfeb5e4e0dce39e4c93ee1b67dc1b6c6c
Vincent de Château-Thierry <vdct@laposte.net>
2022-10-18 22:08:11 +0000

    adaptation pour lancement par dept

--- bano/rapprochement.py
+++ bano/rapprochement.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python
 # coding: UTF-8
 
+from . import db_helpers as h
 from .models import Noms, Adresses, Topo
 
-def process(source,code_insee,dept,**kwargs):
+
+def process_unitaire(source,code_insee):
     topo = Topo(code_insee)
     adresses = Adresses(code_insee,source)
     noms = Noms(code_insee,source)
@@ -19,3 +21,13 @@ def process(source,code_insee,dept,**kwargs):
     noms.add_fantoir(topo)
     adresses.enregistre()
     noms.enregistre()
+
+def process(source,code_insee,dept,**kwargs):
+    if dept:
+        liste_insee = h.liste_communes_par_dept(dept)
+    else:
+        liste_insee = ((code_insee,h.nom_commune(code_insee)))
+    for code_insee,nom in liste_insee:
+        if dept:
+            print(f"{code_insee} - {nom}")
+        process_unitaire(source,code_insee)
