05485bef6ce06c8939e83309304b5596a29e0631
Vincent de Château-Thierry <vdct@laposte.net>
2022-10-21 05:58:21 +0000

    typo sur le nom de fichier des communes pour batch nocturne

--- cron_bano.sh
+++ cron_bano.sh
@@ -34,7 +34,7 @@ then
 else
    bano update_insee_lists
    cat insee_locales.csv  | parallel -j 4 export LANG=fr_FR.UTF-8\; bano process_commune BAL      --code_insee {1}
-   cat insee_osm.txt      | parallel -j 4 export LANG=fr_FR.UTF-8\; bano process_commune BAN      --code_insee {1}
+   cat insee_osm.csv      | parallel -j 4 export LANG=fr_FR.UTF-8\; bano process_commune BAN      --code_insee {1}
    cat insee_osm.csv      | parallel -j 4 export LANG=fr_FR.UTF-8\; bano process_commune OSM      --code_insee {1}
    cat deplist.txt        | parallel -j 4 export LANG=fr_FR.UTF-8\; bano process_commune_lieux-dits    --depts {1}
 fi
