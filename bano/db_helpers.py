acb3836b8c3f0cf990d6bee0242bc2b5bc872ebc
Vincent de Château-Thierry <vdct@laposte.net>
2022-10-23 21:19:28 +0000

    Rapprochement par lots - logs

--- bano/db_helpers.py
+++ bano/db_helpers.py
@@ -6,10 +6,10 @@ from .sql import sql_get_data
 
 
 def liste_communes_par_dept(dept):
-    return sql_get_data('liste_communes_par_dept',dict(dept=dept))
+    return sql_get_data('liste_communes_par_dept',dict(dept=dept),db.bano)
 
 def nom_commune(code_insee):
-    return sql_get_data('nom_commune_par_code_insee',dict(code_insee=code_insee))
+    return sql_get_data('nom_commune_par_code_insee',dict(code_insee=code_insee),db.bano)[0][0]
 
 def get_cadastre_format(insee_com):
     with db.bano.cursor() as conn :
--- bano/rapprochement.py
+++ bano/rapprochement.py
@@ -2,31 +2,38 @@
 # coding: UTF-8
 
 from . import db_helpers as h
+from . import batch as b
 from .models import Noms, Adresses, Topo
 
 
 def process_unitaire(source,code_insee):
-    topo = Topo(code_insee)
-    adresses = Adresses(code_insee,source)
-    noms = Noms(code_insee,source)
+    id_batch = b.batch_start_log('rapprochement', source, code_insee)
+    try:
+        topo = Topo(code_insee)
+        adresses = Adresses(code_insee,source)
+        noms = Noms(code_insee,source)
 
-    if source == 'OSM':
-        noms.charge_noms_osm_hors_numeros()
-        adresses.charge_numeros_osm()
+        if source == 'OSM':
+            noms.charge_noms_osm_hors_numeros()
+            adresses.charge_numeros_osm()
 
-    if source == 'BAN':
-        adresses.charge_numeros_ban(topo)
+        if source == 'BAN':
+            adresses.charge_numeros_ban(topo)
 
-    adresses.noms_des_adresses(noms)
-    noms.add_fantoir(topo)
-    adresses.enregistre()
-    noms.enregistre()
+        adresses.noms_des_adresses(noms)
+        noms.add_fantoir(topo)
+        adresses.enregistre()
+        noms.enregistre()
+        b.batch_stop_log(id_batch,True)
+    except:
+        b.batch_stop_log(id_batch,False)
+
 
 def process(source,code_insee,dept,**kwargs):
     if dept:
         liste_insee = h.liste_communes_par_dept(dept)
     else:
-        liste_insee = ((code_insee,h.nom_commune(code_insee)))
+        liste_insee = [(code_insee,h.nom_commune(code_insee))]
     for code_insee,nom in liste_insee:
         if dept:
             print(f"{code_insee} - {nom}")
