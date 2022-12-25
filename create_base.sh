1b4e1d06b78436c48b9323e3f139195fc021e733
Frédéric Rodrigo <fred.rodrigo@gmail.com>
2022-12-25 23:24:57 +0100

    Fix create_base.sql sql script path (#318)

--- create_base.sh
+++ create_base.sh
@@ -6,7 +6,7 @@ sudo -u postgres -s
 createuser -s -d cadastre
 createdb -E UTF-8 -T template0 -O cadastre cadastre
 exit
-psql -d cadastre -U cadastre -v ON_ERROR_STOP=1 -f sql/create_base.sql
+psql -d cadastre -U cadastre -v ON_ERROR_STOP=1 -f bano/sql/create_base.sql
 psql -d cadastre -U cadastre -v ON_ERROR_STOP=1 -f sql/create_table_base_bano.sql
-psql -d osm -U cadastre -v ON_ERROR_STOP=1 -f sql/create_base.sql
+psql -d osm -U cadastre -v ON_ERROR_STOP=1 -f bano/sql/create_base.sql
 psql -d osm -U cadastre -v ON_ERROR_STOP=1 -f sql/create_table_base_osm.sql
