d1a2bf82f9ba62a0300429c025999403e2df141a
Frédéric Rodrigo <fred.rodrigo@gmail.com>
2022-12-25 19:03:21 +0100

    Add argument to load_osm_france_db

--- load_osm_france_db.sh
+++ load_osm_france_db.sh
@@ -3,6 +3,9 @@
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source $SCRIPT_DIR/config
 
+PBF_URL=${1:-http://download.openstreetmap.fr/extracts/merge/france_metro_dom_com_nc.osm.pbf}
+PBF_FILE=$(basename "$PBF_URL")
+
 lockfile=${SCRIPT_DIR}/imposm.lock
 
 if test -f ${lockfile}
@@ -13,9 +16,10 @@ fi
 
 touch ${lockfile}
 
+mkdir -p $DOWNLOAD_DIR
 cd $DOWNLOAD_DIR
-wget -NS http://download.openstreetmap.fr/extracts/merge/france_metro_dom_com_nc.osm.pbf
-imposm import -mapping $BANO_DIR/bano.yml -read $DOWNLOAD_DIR/france_metro_dom_com_nc.osm.pbf -overwritecache -cachedir $IMPOSM_CACHE_DIR -dbschema-import public -diff -diffdir $DOWNLOAD_DIR
+wget -NS $PBF_URL
+imposm import -mapping $BANO_DIR/bano.yml -read $DOWNLOAD_DIR/$PBF_FILE -overwritecache -cachedir $IMPOSM_CACHE_DIR -dbschema-import public -diff -diffdir $DOWNLOAD_DIR
 imposm import -mapping $BANO_DIR/bano.yml -write -connection 'postgis://cadastre@localhost/osm'?prefix=NONE -cachedir $IMPOSM_CACHE_DIR -dbschema-import public -diff -diffdir $DOWNLOAD_DIR
 
 psql -d osm -U cadastre -v ON_ERROR_STOP=1 -f $BANO_DIR/sql/finalisation.sql
