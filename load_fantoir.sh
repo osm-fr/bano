b48dec0765a1edcf58f88b61e880d294a2cc0a19
Frédéric Rodrigo <fred.rodrigo@gmail.com>
2022-12-26 23:49:27 +0100

    Make load script runnable from scratch and fix wget usage (#327)

--- load_fantoir.sh
+++ load_fantoir.sh
@@ -6,7 +6,7 @@ source config
 cd $DATA_DIR
 mkdir -p fantoir
 cd fantoir
-rm -rf FANTOIR* && rm *.zip
+rm -rf FANTOIR* && rm -f *.zip
 wget https://data.economie.gouv.fr/api/datasets/1.0/fichier-fantoir-des-voies-et-lieux-dits/attachments/fichier_national_fantoir_situation_novembre_2022_zip/ -O fantoir.zip
 unzip fantoir.zip
 mv FANTOIR* fantoir.txt
