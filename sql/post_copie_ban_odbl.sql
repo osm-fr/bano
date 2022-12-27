6e10367620e2920ebe04acfd5b7f18a371a16004
Frédéric Rodrigo <fred.rodrigo@gmail.com>
2022-12-27 10:05:38 +0100

    Make post_copie_ban_odbl.sql re-runable

--- sql/post_copie_ban_odbl.sql
+++ sql/post_copie_ban_odbl.sql
@@ -1,3 +1,3 @@
-ALTER TABLE ban_odbl ADD COLUMN geometrie geometry(Point, 4326);
+ALTER TABLE ban_odbl ADD COLUMN IF NOT EXISTS geometrie geometry(Point, 4326);
 UPDATE ban_odbl SET geometrie = ST_SetSRID(ST_Point(lon,lat),4326);
-CREATE INDEX gidx_ban_odbl ON ban_odbl USING GIST(geometrie);
+CREATE INDEX IF NOT EXISTS gidx_ban_odbl ON ban_odbl USING GIST(geometrie);
