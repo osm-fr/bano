2613149ba6344d174f392e8c33bb9f78865c576b
deuzeffe <36087963+deuzeffe@users.noreply.github.com>
2023-02-14 21:09:57 +0100

    Update load_fantoir.sh (#338)
    Nouveau fichier fantoir T1 2023 : BANZAI :champagne:

--- load_fantoir.sh
+++ load_fantoir.sh
@@ -7,7 +7,7 @@ cd $DATA_DIR
 mkdir -p fantoir
 cd fantoir
 rm -rf FANTOIR* && rm -f *.zip
-wget https://data.economie.gouv.fr/api/datasets/1.0/fichier-fantoir-des-voies-et-lieux-dits/attachments/fichier_national_fantoir_situation_novembre_2022_zip/ -O fantoir.zip
+wget https://data.economie.gouv.fr/api/datasets/1.0/fichier-fantoir-des-voies-et-lieux-dits/attachments/fichier_national_fantoir_situation_janvier_2023_zip/ -O fantoir.zip
 unzip fantoir.zip
 mv FANTOIR* fantoir.txt
 psql -d osm -U cadastre -v ON_ERROR_STOP=1 -f $BANO_DIR/sql/load_fantoir.sql
