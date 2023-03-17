28d6cfafb42be9873774e35df08c466e089efb0b
Vincent de Château-Thierry <vdct@laposte.net>
2023-03-17 12:10:37 +0000

    Base unique

--- bano/sql/create_base.sql
+++ bano/sql/create_base.sql
@@ -1,5 +1,5 @@
 CREATE EXTENSION IF NOT EXISTS postgis;
 CREATE EXTENSION IF NOT EXISTS hstore;
 
-# tables Imposm dans le schema osm
+-- tables Imposm dans le schema osm
 ALTER ROLE cadastre IN DATABASE bano SET search_path TO public,osm;
