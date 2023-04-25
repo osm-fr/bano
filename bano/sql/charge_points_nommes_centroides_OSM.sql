50d170554a2a4ae7bdaaed39ea6413d5457667f4
Vincent de Château-Thierry <vdct@laposte.net>
2023-04-25 19:16:46 +0000

    complement centroides

--- bano/sql/charge_points_nommes_centroides_OSM.sql
+++ bano/sql/charge_points_nommes_centroides_OSM.sql
@@ -14,21 +14,54 @@ LEFT OUTER JOIN (SELECT * FROM polygones_insee_a9 WHERE insee_a8 = '__code_insee
 ON      ST_Intersects(l.way, a9.geometrie)
 WHERE   (l.highway != '' OR
         l.waterway = 'dam')     AND
+        l.name != ''
+UNION ALL
+SELECT  ST_PointOnSurface(l.way),
+        unnest(array[l.name,l.tags->'alt_name',l.tags->'old_name']) AS name,
+        COALESCE(a9.code_insee,'xxxxx') as insee_jointure,
+        a9.code_insee insee_ac,
+        "ref:FR:FANTOIR" AS fantoir,
+        ST_Within(l.way,p.geometrie)::integer as within
+FROM    (SELECT geometrie FROM polygones_insee WHERE code_insee = '__code_insee__') p
+JOIN    planet_osm_polygon l
+ON      ST_Intersects(l.way, p.geometrie)
+LEFT OUTER JOIN (SELECT * FROM polygones_insee_a9 WHERE insee_a8 = '__code_insee__') a9
+ON      ST_Intersects(l.way, a9.geometrie)
+WHERE   (l.highway||"ref:FR:FANTOIR" != '' OR l.landuse = 'residential' OR l.amenity = 'parking') AND
+        l.name != ''
+UNION ALL
+SELECT l.way,
+        unnest(array[l.name,l.tags->'alt_name',l.tags->'old_name']) AS name,
+        COALESCE(a9.code_insee,'xxxxx') as insee_jointure,
+        a9.code_insee insee_ac,
+        "ref:FR:FANTOIR" AS fantoir,
+        ST_Within(l.way,p.way)::integer as within
+FROM    (SELECT way FROM planet_osm_polygon WHERE "ref:INSEE" = '__code_insee__') p
+JOIN    planet_osm_rels l
+ON      ST_Intersects(l.way, p.way)
+LEFT OUTER JOIN (SELECT * FROM polygones_insee_a9 WHERE insee_a8 = '__code_insee__') a9
+ON      ST_Intersects(l.way, a9.geometrie)
+WHERE   l.member_role = 'street' AND
         l.name != ''),
 lignes_noms
 AS
-(SELECT *
+(SELECT CASE 
+            WHEN GeometryType(way) = 'POINT' THEN ST_MakeLine(ST_Translate(way,-0.000001,-0.000001),ST_Translate(way,0.000001,0.000001))
+            WHEN GeometryType(way) LIKE '%POLYGON' THEN ST_ExteriorRing(way)
+            ELSE way
+        END AS way_line,
+        *
 FROM    lignes_brutes
 WHERE   name IS NOT NULL AND
         (fantoir LIKE '__code_insee__%' OR fantoir = '')),
 lignes_noms_rang
 AS
 (SELECT *,
-        RANK() OVER(PARTITION BY name,insee_ac ORDER BY within DESC) rang
+        RANK() OVER(PARTITION BY name,insee_ac ORDER BY within DESC, fantoir DESC) rang
 FROM    lignes_noms),
 lignes_agregees
 AS
-(SELECT ST_LineMerge(ST_Collect(way)) way,
+(SELECT ST_LineMerge(ST_Collect(way_line)) way,
         name,
         insee_ac,
         insee_jointure,
@@ -38,7 +71,7 @@ WHERE   rang = 1
 GROUP BY 2,3,4,5),
 centroide_lignes_agregees
 AS
-(SELECT ST_Centroid(ST_LineMerge(ST_Collect(way))) way,
+(SELECT ST_Centroid(ST_LineMerge(ST_Collect(way_line))) way,
         name,
         insee_ac,
         insee_jointure,
