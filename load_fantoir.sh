77eec31b337c7c9deef54594257a68185b5813bd
deuzeffe <36087963+deuzeffe@users.noreply.github.com>
2023-06-04 22:04:23 +0200

    Update load_fantoir.sh
    màj url de l'après-dernier fichier fantoir (avril 2023)

--- load_fantoir.sh
+++ load_fantoir.sh
@@ -7,7 +7,7 @@ cd $DATA_DIR
 mkdir -p fantoir
 cd fantoir
 rm -rf FANTOIR* && rm -f *.zip
-wget https://data.economie.gouv.fr/api/datasets/1.0/fichier-fantoir-des-voies-et-lieux-dits/attachments/fichier_national_fantoir_situation_janvier_2023_zip/ -O fantoir.zip
+wget https://data.economie.gouv.fr/api/datasets/1.0/fichier-fantoir-des-voies-et-lieux-dits/attachments/fichier_national_fantoir_situation_avril_2023_zip/ -O fantoir.zip
 unzip fantoir.zip
 mv FANTOIR* fantoir.txt
 $pgsql_OSM -f $BANO_DIR/sql/load_fantoir.sql
