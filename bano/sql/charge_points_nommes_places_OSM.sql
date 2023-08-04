5126b068632121b39c085ef97c8b5bbc64ec3f4f
Vincent de Château-Thierry <vdct@laposte.net>
2023-08-04 13:06:16 +0000

    Suppression des noms d'admin_centre pour les rapprochements (#364)

--- bano/sql/charge_points_nommes_places_OSM.sql
+++ bano/sql/charge_points_nommes_places_OSM.sql
@@ -12,8 +12,11 @@ FROM    (SELECT way FROM planet_osm_polygon WHERE "ref:INSEE" = '__code_insee__'
 JOIN    (SELECT * FROM planet_osm_point WHERE place != '' AND name != '') pt
 ON      pt.way && p.way                 AND
          ST_Intersects(pt.way, p.way)
+LEFT OUTER JOIN (SELECT osm_id FROM planet_osm_communes_statut WHERE "ref:INSEE" = '__code_insee__' AND member_role = 'admin_centre') admin_centre
+ON      pt.osm_id = admin_centre.osm_id
 LEFT OUTER JOIN (SELECT * FROM polygones_insee_a9 WHERE insee_a8 = '__code_insee__') a9
-ON      ST_Intersects(pt.way, a9.geometrie))
+ON      ST_Intersects(pt.way, a9.geometrie)
+WHERE   admin_centre.osm_id IS NULL)
 SELECT  ST_x(way),
         ST_y(way),
         name,
