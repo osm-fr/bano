01116821ab91f6d950b0897f46daeb7b940405f0
Vincent de Château-Thierry <vdct@laposte.net>
2023-11-19 23:30:23 +0000

    COG 2023

--- bano/sources/cog.py
+++ bano/sources/cog.py
@@ -1,76 +1,76 @@
-# import csv
-from zipfile import ZipFile
 import os
-# import subprocess
-# from datetime import datetime
+
 from email.utils import formatdate, parsedate_to_datetime
 from pathlib import Path
 
 import requests
-# import psycopg2
 
 from ..db import bano_db
-from ..sql import sql_process
 from .. import batch as b
 
-def process_cog(**kwargs):
-    zip = get_destination('cog.zip')
-    status = download(zip)
-    if status:
-        import_to_pg(zip)
+DICT_COG = {
+    "commune": [
+        "cog_commune",
+        "https://www.data.gouv.fr/fr/datasets/r/5d5c4384-b19a-4ddf-910b-6e9cbaa8e1d6",
+    ],
+    "canton": [
+        "cog_canton",
+        "https://www.data.gouv.fr/fr/datasets/r/716922e8-2ab7-4806-94eb-21933c266ebf",
+    ],
+    "departement": [
+        "cog_departement",
+        "https://www.data.gouv.fr/fr/datasets/r/c691a0b0-6e22-4d6f-9496-3406b017d709",
+    ],
+    "region": [
+        "cog_region",
+        "https://www.data.gouv.fr/fr/datasets/r/6e40e6f3-7735-43a9-8f20-ce0ae53f74f7",
+    ],
+}
 
-def download(destination):
+
+def process_cog(**kwargs):
+    for k,v in DICT_COG.items():
+        print(f"Chargement de la source COG {k}")
+        table,url = v
+        csv = get_destination(f"{k}.csv")
+        status = download(csv,url)
+        if status:
+            import_to_pg(csv,table)
+
+
+def download(destination,url):
     headers = {}
     if destination.exists():
-        headers['If-Modified-Since'] = formatdate(destination.stat().st_mtime)
+        headers["If-Modified-Since"] = formatdate(destination.stat().st_mtime)
 
-    resp = requests.get(get_COG_URL(), headers=headers)
-    id_batch = b.batch_start_log('download source', 'COG ZIP','France')
+    resp = requests.get(url, headers=headers)
+    id_batch = b.batch_start_log("download source", f"COG {destination}", "France")
     if resp.status_code == 200:
-        with destination.open('wb') as f:
+        with destination.open("wb") as f:
             f.write(resp.content)
-        # mtime = parsedate_to_datetime(resp.headers['Last-Modified']).timestamp()
-        # os.utime(destination, (mtime, mtime))
-        b.batch_stop_log(id_batch,True)
+        b.batch_stop_log(id_batch, True)
         return True
     print(resp.status_code)
-    b.batch_stop_log(id_batch,False)
+    b.batch_stop_log(id_batch, False)
     return False
 
 
-def import_to_pg(fichier_zip):
-    table = 'cog_commune'
-    id_batch = b.batch_start_log('import source', f'COG {table}','France')
-    with ZipFile(fichier_zip) as f:
-        with f.open(get_COG_CSV()) as csv:
-            csv.readline()  # skip CSV headers
-            with bano_db.cursor() as cur_insert:
-                try:
-                    cur_insert.execute(f"TRUNCATE {table}")
-                    cur_insert.copy_from(csv,table, sep=',', null='')
-                    b.batch_stop_log(id_batch,True)
-                except psycopg2.DataError as e:
-                    b.batch_stop_log(id_batch,False)
-    
+def import_to_pg(csv,table):
+    id_batch = b.batch_start_log("import source", f"COG {table}", "France")
+    with open(csv) as f:
+        with bano_db.cursor() as cur_insert:
+            try:
+                cur_insert.execute(f"TRUNCATE {table}")
+                cur_insert.copy_expert(f"COPY {table} FROM STDIN WITH CSV HEADER",f)
+                b.batch_stop_log(id_batch, True)
+            except psycopg2.DataError as e:
+                b.batch_stop_log(id_batch, False)
+
 def get_destination(fichier_cog):
     try:
-        cwd = Path(os.environ['COG_DIR'])
+        cwd = Path(os.environ["COG_DIR"])
     except KeyError:
         raise ValueError(f"La variable COG_DIR n'est pas définie")
     if not cwd.exists():
         raise ValueError(f"Le répertoire {cwd} n'existe pas")
-    return cwd / f'{fichier_cog}'
-
-def get_COG_URL():
-    try:
-        url = os.environ['COG_URL']
-    except KeyError:
-        raise ValueError(f"La variable COG_URL n'est pas définie")
-    return url
-
-def get_COG_CSV():
-    try:
-        csv = os.environ['COG_CSV_COMMUNE']
-    except KeyError:
-        raise ValueError(f"La variable COG_CSV_COMMUNE n'est pas définie")
-    return csv
+    return cwd / f"{fichier_cog}"
