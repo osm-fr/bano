824020daf3c08f514286115f09dcfa8aff7eda4f
Frédéric Rodrigo <fred.rodrigo@gmail.com>
2024-02-14 18:18:07 +0100

    Does not log from script

--- cron_bano.sh
+++ cron_bano.sh
@@ -4,10 +4,7 @@ set -e
 
 source /data/project/bano_v3/venv_v3/bin/activate
 
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-cd $SCRIPT_DIR
-
-echo 'debut du cron BANO' >> $SCRIPT_DIR/cron.log
+echo 'debut du cron BANO'
 
 source config
 
@@ -20,7 +17,7 @@ cat deplist.txt | parallel -j 4 export LANG=$LANG\; bano charge_ban {1}
 cat deplist.txt | parallel -j 4 export LANG=$LANG\; bano charge_ld_cadastre {1}
 bano update_bis_table
 
-echo 'sources ok' >> $SCRIPT_DIR/cron.log
+echo 'sources ok'
 
 # Mise à jour quotidienne dans la base cadastre des couches des polygones postaux d'OSM et des statuts admin de communes en vue des exports
 bano update_table_communes
@@ -28,21 +25,21 @@ bano update_infos_communes
 
 # $pgsql_BANO -f sql/create_table_polygones_postaux.sql
 # $pgsql_CADASTRE -f sql/post_copie_ban.sql
-echo 'màj polygones ok' >> $SCRIPT_DIR/cron.log
+echo 'màj polygones ok'
 
 # BANO
 cat deplist.txt        | parallel -j 4 export LANG=$LANG\; bano rapprochement --dept {1}
 
-echo 'rapprochement ok' >> $SCRIPT_DIR/cron.log
+echo 'rapprochement ok'
 
-echo 'preparation export' >> $SCRIPT_DIR/cron.log
+echo 'preparation export'
 bano prepare_export
-echo 'preparation export finie' >> $SCRIPT_DIR/cron.log
+echo 'preparation export finie'
 
 # exports
-echo 'export' >> $SCRIPT_DIR/cron.log
+echo 'export'
 cat deplist.txt | parallel -j 4 bano export {1}
-echo 'export fini' >> $SCRIPT_DIR/cron.log
+echo 'export fini'
 
 # copie+zip dans le dossier web
 cat deplist.txt | parallel -j 4 bano publish {1}
@@ -53,4 +50,4 @@ $pgsql_BANO -c "VACUUM bano_adresses;"
 $pgsql_BANO -c "VACUUM bano_points_nommes;"
 $pgsql_BANO -c "VACUUM nom_fantoir;"
 $pgsql_BANO -c "GRANT SELECT ON ALL TABLES IN SCHEMA PUBLIC TO PUBLIC";
-echo 'fin du cron BANO' >> $SCRIPT_DIR/cron.log
+echo 'fin du cron BANO'
