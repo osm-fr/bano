e1fb1d1990477a0b0d7fc23c3259f15c8343bd55
Frédéric Rodrigo <fred.rodrigo@gmail.com>
2024-02-15 16:51:41 +0100

    Load the download file with imposm

--- load_osm_france_db.sh
+++ load_osm_france_db.sh
@@ -21,7 +21,7 @@ touch ${lockfile}
 mkdir -p $DOWNLOAD_DIR
 cd $DOWNLOAD_DIR
 wget -NS $PBF_URL
-imposm import -config $SCRIPT_DIR/imposm.config -read $DOWNLOAD_DIR/france_metro_dom_com_nc.osm.pbf -overwritecache -diff -write -dbschema-import osm
+imposm import -config $SCRIPT_DIR/imposm.config -read $DOWNLOAD_DIR/$PBF_FILE -overwritecache -diff -write -dbschema-import osm
 
 psql -d bano -U cadastre -f $SCRIPT_DIR/sql/finalisation.sql
 
