23b64284194bdc2c1c0dd8c1355e0ef555dc71c1
Frédéric Rodrigo <fred.rodrigo@gmail.com>
2024-07-08 22:14:47 +0200

    Only one source for deps (#389)

--- utils/load_csv_export.sh
+++ utils/load_csv_export.sh
@@ -2,6 +2,11 @@
 
 set -e
 
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd $SCRIPT_DIR
+
+source config
+
 psql -d cadastre -U cadastre -v ON_ERROR_STOP=1 -c "
   DROP TABLE IF EXISTS export_csv CASCADE; \
   CREATE TABLE export_csv(
@@ -14,7 +19,7 @@ psql -d cadastre -U cadastre -v ON_ERROR_STOP=1 -c "
   lat decimal,
   lon decimal);"
 
-for dep in {01..19} 2A 2B {21..95} {971..974} 976
+for dep in `cat $BANO_DIR/deplist.txt`
 do
   cat /data/sas_web/bano-${dep}.csv| psql -d cadastre -U cadastre -c "COPY export_csv FROM STDIN WITH CSV"
 done
